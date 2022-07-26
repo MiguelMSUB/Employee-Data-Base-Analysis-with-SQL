6c1f6b217d60fcec51f87ee0de521e7e601d421c
MiguelMSUB <miguel.apc@outlook.com>
2022-07-26 14:30:42 -0400

    Challenge update Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -6,8 +6,8 @@ SELECT e.emp_no,
 	ti.from_date,
 	ti.to_date
 INTO retirement_titles
-FROM employees as e
-INNER JOIN titles as ti
+FROM employees AS e
+INNER JOIN titles AS ti
 ON (e.emp_no = ti.emp_no)
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
@@ -29,3 +29,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+-- Mentorship Eligibility
+
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+	e.first_name, 
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	ti.title
+INTO mentorship_eligibilty
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	 AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
